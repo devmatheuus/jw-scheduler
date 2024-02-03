ff5491221eddcbbce5cf2dfaa9f6f43ec282fe32
Matheus Silva <matheus_lsilva@hotmail.com>
2024-02-03 11:50:13 -0300

    Remove Docker-related files and update dependencies

--- src/services/christians/getChristiansService.ts
+++ src/services/christians/getChristiansService.ts
@@ -1,6 +1,5 @@
 import { appDataSource } from '../../dataSource';
 import { ChristianRoles } from '../../types/christian/roles';
-import redis from '../../lib/redis-client';
 
 type GetChristianServiceProps = {
   role?: ChristianRoles;
@@ -12,79 +11,39 @@ export const getChristiansService = async ({
   role,
 }: GetChristianServiceProps) => {
   const christianRepository = appDataSource.getRepository('christians');
-  let cacheKey = '';
 
   if (!gender && !role) {
-    cacheKey = 'getChristiansService';
-    const cachedData = await redis.get(cacheKey);
-
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    }
-
     const christians = await christianRepository.find();
-
-    await redis.set(cacheKey, JSON.stringify(christians));
 
     return christians;
   }
 
   if (gender && role) {
-    cacheKey = `getChristiansService-${gender}-${role}`;
-
-    const cachedData = await redis.get(cacheKey);
-
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    }
-
     const christians = await christianRepository.find({
       where: {
         gender,
         roles: '["' + role + '"]',
       },
     });
-
-    await redis.set(cacheKey, JSON.stringify(christians));
 
     return christians;
   }
 
   if (!gender && role) {
-    cacheKey = `getChristiansService-${role}`;
-
-    const cachedData = await redis.get(cacheKey);
-
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    }
-
     const christians = await christianRepository.find({
       where: {
         roles: '["' + role + '"]',
       },
     });
 
-    await redis.set(cacheKey, JSON.stringify(christians));
-
     return christians;
   }
   if (gender && !role) {
-    cacheKey = `getChristiansService-${gender}`;
-
-    const cachedData = await redis.get(cacheKey);
-
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    }
-
     const christians = await christianRepository.find({
       where: {
         gender,
       },
     });
-
-    await redis.set(cacheKey, JSON.stringify(christians));
 
     return christians;
   }
--- src/services/christians/getOneChristianService.ts
+++ src/services/christians/getOneChristianService.ts
@@ -1,22 +1,11 @@
-import redis from '../../lib/redis-client';
 import { appDataSource } from '../../dataSource';
 import { NotFoundError } from '../../errors/NotFoundError';
 
 export const getOneChristianService = async (id: string) => {
-  const cacheKey = `getOneChristianService-${id}`;
-
-  const cachedData = await redis.get(cacheKey);
-
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
-
   const christianRepository = appDataSource.getRepository('christians');
   const christian = await christianRepository.findOne({ where: { id } });
 
   if (!christian) throw new NotFoundError('Christian not found');
-
-  await redis.set(cacheKey, JSON.stringify(christian));
 
   return christian;
 };
